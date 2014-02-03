b343d9835ccdbf58a31f01ba3bbe3a612b3c7fe6
S.Wang <will.sizhou.wang@gmail.com>
2014-02-02 20:43:05 -0600

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
+//var webpage = fs.readFileSync("index.html", "utf-8");
+
+
+var buffer = fs.readFileSync("index.html");
+var webpage = buffer.toString();
+
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  response.send(webpage);
 });
 
 var port = process.env.PORT || 5000;
